5914b3db1757e384079058751d81b17322f57f85
Gabriel Salas <ggsalas@gmail.com>
2023-03-09 22:11:36 -0300

    feat: Get container values using resizeObserver

--- app/components/PagedNavigation/useContainerValues.ts
+++ app/components/PagedNavigation/useContainerValues.ts
@@ -1,10 +1,11 @@
 import { useEffect, useRef, useState } from "react";
+import { exists } from "~/shared/common";
 
 type ContainerValues = {
   articleColumnWidth: string;
   articlePadding: string;
-  viewportWidth: string;
-  viewportHeight: string;
+  viewportWidth?: number;
+  viewportHeight?: number;
   increment: number;
 };
 
@@ -18,33 +19,53 @@ export function useContainerValues() {
   const [containerValues, setContainerValues] = useState<ContainerValues>({
     articleColumnWidth: "0px",
     articlePadding: "0px",
-    viewportWidth: "0px",
-    viewportHeight: "0px",
+    viewportWidth: 0,
+    viewportHeight: 0,
     increment: 0,
   });
 
+  // get css values from the DOM
   useEffect(() => {
     if (containerElement) {
       const elementStyles = window.getComputedStyle(
         containerElement as Element
       );
-      const body = document.querySelector("body");
-      const bodyStyles = body ? window.getComputedStyle(body) : null;
 
       const articleColumnWidth = getValue("width", elementStyles);
       const articlePadding = getValue("--articlePadding", elementStyles);
-      const viewportWidth = getValue("width", bodyStyles);
-      const viewportHeight = getValue("height", bodyStyles);
 
       setContainerValues({
         articleColumnWidth,
         articlePadding,
-        viewportWidth,
-        viewportHeight,
         increment: parseInt(articleColumnWidth) + parseInt(articlePadding),
       });
     }
   }, [containerElement]);
+
+  // get viewport size
+  useEffect(() => {
+    const node = document.querySelector("body");
+
+    if (node !== null) {
+      const resizeObserver = new ResizeObserver((entries) => {
+        const entry = entries[0];
+
+        const viewportWidth = entry.target.getBoundingClientRect().width;
+        const viewportHeight = entry.target.getBoundingClientRect().height;
+
+        exists(viewportWidth) &&
+          setContainerValues((st) => ({ ...st, viewportWidth }));
+        exists(viewportHeight) &&
+          setContainerValues((st) => ({ ...st, viewportHeight }));
+      });
+
+      resizeObserver.observe(node);
+
+      return () => {
+        resizeObserver.unobserve(node);
+      };
+    }
+  }, []);
 
   return {
     containerRef,
